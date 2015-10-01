97e261974771d38a1d1ffb007ae4c526484e0fff
kspiesclimate <katie.spies@climate.com>
2015-10-01 14:05:24 -0700

    Initial Commit

---
+++ Week 3 Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 3 Lab
+//
+//  Created by Katie Spies on 10/1/15.
+//  Copyright (c) 2015 Katie Spies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
